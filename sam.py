73b9d07e737fc698849a85ed64112062faba85f3
immoh4n <mohan960035@gmail.com>
2025-03-05 12:10:45 +0530

    repush

--- sam.py
+++ sam.py
@@ -1,12 +1,1 @@
-def add(a, b):
-    return a + b
-def add_three_numbers(a, b, c):
-    return a + b + c
-
-def main():
-    print(add(1, 2))
-
-    print(add_three_numbers(1, 2, 3))
-
-if __name__ == "__main__":
-    main()
+print("hello world")
